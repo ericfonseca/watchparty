c12c4b43261f70ac9a5f485f2b7c3caf6a7af889
eric <edavidfonseca@gmail.com>
2018-07-17 00:40:20 -0700

    add sql schema, readme, and api spec

---
+++ README.md
@@ -0,0 +1,7 @@
+# installing postgres
+* brew install postgres
+* brew services start postgresql
+* psql postgres -U <username>
+* CREATE DATABASE watch_party;
+* /c watch_party;
+* execute commands in schema.sql
---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE watch_party;
+
+CREATE TABLE users (
+id SERIAL,
+name VARCHAR(100),
+email VARCHAR(100),
+sport_interests VARCHAR(200),
+city_interests VARCHAR(200));
+
+CREATE TABLE events (
+id SERIAL,
+title VARCHAR(100),
+sport VARCHAR(100),
+city VARCHAR(100),
+start_time DATE);
+
+CREATE TABLE venues (
+id SERIAL,
+city VARCHAR(100), address VARCHAR(150),
+description VARCHAR(300));
+
+CREATE TABLE watchers (
+event_id INT,
+user_id INT);
+
+CREATE TABLE hosters (
+event_id INT,
+venue_id INT);
